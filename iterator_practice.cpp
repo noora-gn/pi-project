1ffd1e5d5d7b364909fc8791cb960edff4394c0f
Geoffrey Lovelace <geoffrey4444@gmail.com>
2021-03-09 09:00:13 -0800

    Suggestions to polish iterator practice

--- iterator_practice.cpp
+++ iterator_practice.cpp
@@ -1,16 +1,13 @@
 #include <iostream>
-#include <iterator> // for iterator
 #include <vector> // for vectors 
 
-using namespace std;
-
-int main()
-{
-    vector<int> ar = {2,4,6,8,10};
-    vector<int> ::iterator ptr;
-    cout << "My vector elements are: ";
-    for (ptr = ar.begin(); ptr < ar.end (); ptr++)
-        cout << *ptr<< " ";
-
-    return 0; 
+int main() {
+  const std::vector<int> evens{2,4,6,8,10};
+  std::cout << "My vector elements are: ";
+  for (auto it = evens.begin(); it < evens.end(); ++it) {
+    std::cout << *it << " ";
+  }
+  std::cout << "\n";
+  
+  return 0; 
 }
